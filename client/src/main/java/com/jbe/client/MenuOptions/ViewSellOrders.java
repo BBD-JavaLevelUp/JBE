b71bf6ed2e982c6b506d5f609359420f075e0aae
tebogo-bbd <159124930+tebogo-bbd@users.noreply.github.com>
2024-03-07 14:44:10 +0200

    add getAllSellOrders mock function on RestApiHandler

--- client/src/main/java/com/jbe/client/MenuOptions/ViewSellOrders.java
+++ client/src/main/java/com/jbe/client/MenuOptions/ViewSellOrders.java
@@ -1,30 +1,20 @@
 package com.jbe.client.MenuOptions;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Scanner;
 
 import com.jbe.client.MainMenu;
+import com.jbe.client.RestApiHandler;
 import com.jbe.client.Models.Investor;
 import com.jbe.client.Models.SellOrder;
 
 public class ViewSellOrders{
     private static Scanner scanner = new Scanner(System.in);
     public Investor currentInvestor;
-    public ArrayList<SellOrder> sellOrders = new ArrayList<>();
+    public ArrayList<SellOrder> sellOrders = RestApiHandler.getAllSellOrders();
 
 
     public ViewSellOrders(Investor currentInvestor){
         this.currentInvestor = currentInvestor;
-        // Creating SellOrder instances
-        SellOrder sellOrder1 = new SellOrder(1, 101, 201, 10.50, 50, 100, new Date(), true);
-        SellOrder sellOrder2 = new SellOrder(2, 102, 202, 12.75, 30, 50, new Date(), true);
-        SellOrder sellOrder3 = new SellOrder(3, 103, 203, 9.25, 40, 80, new Date(), true);
-
-        // Adding SellOrder instances to the ArrayList
-        sellOrders.add(sellOrder1);
-        sellOrders.add(sellOrder2);
-        sellOrders.add(sellOrder3);
-
     }
 
     public void display() {
@@ -68,7 +58,7 @@ public class ViewSellOrders{
                 MainMenu.display(currentInvestor);
             }
             SellOrder sellOrder = sellOrders.get(choice-1);
-            System.out.println("Sell order accepted");
+                        System.out.println("Sell order accepted");
             MainMenu.display(currentInvestor);
         }
     }
--- client/src/main/java/com/jbe/client/RestApiHandler.java
+++ client/src/main/java/com/jbe/client/RestApiHandler.java
@@ -1,5 +1,26 @@
 package com.jbe.client;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.jbe.client.Models.SellOrder;
+
 public class RestApiHandler {
+
+    public static ArrayList<SellOrder> getAllSellOrders() {
+        ArrayList<SellOrder> sellOrders = new ArrayList<>();
+
+        // Creating SellOrder instances
+        SellOrder sellOrder1 = new SellOrder(1, 101, 201, 10.50, 50, 100, new Date(), true);
+        SellOrder sellOrder2 = new SellOrder(2, 102, 202, 12.75, 30, 50, new Date(), true);
+        SellOrder sellOrder3 = new SellOrder(3, 103, 203, 9.25, 40, 80, new Date(), true);
+
+        // Adding SellOrder instances to the ArrayList
+        sellOrders.add(sellOrder1);
+        sellOrders.add(sellOrder2);
+        sellOrders.add(sellOrder3);
+        
+        return sellOrders;
+    }
     
 }
