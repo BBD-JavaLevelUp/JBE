f701f03552202adc1603363c6be2584af6c0f42c
Abdul Gany Osman <Abdul.Osman@bbd.co.za>
2024-03-07 22:44:27 +0200

    Added entities

--- server/src/main/java/com/jbe/entity/Bean.java
+++ server/src/main/java/com/jbe/entity/Bean.java
@@ -4,51 +4,58 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.math.BigDecimal;
 
 @Entity
 public class Bean {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int beanId;
     private String name;
-    private Float defaultPrice;
+    private BigDecimal defaultPrice;
 
     public Bean() {
     }
 
-    public Bean(String name, Float defaultPrice) {
+    public Bean(int beanId, String name, BigDecimal defaultPrice) {
+        this.beanId = beanId;
         this.name = name;
         this.defaultPrice = defaultPrice;
     }
 
-    public Long getId() {
-        return id;
+    public Bean(String name, BigDecimal defaultPrice) {
+        this.name = name;
+        this.defaultPrice = defaultPrice;
+    }
+
+    public int getBeanId() {
+        return beanId;
     }
 
     public String getName() {
         return name;
     }
 
-    public Float getDefaultPrice() {
+    public BigDecimal getDefaultPrice() {
         return defaultPrice;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setBeanId(int beanId) {
+        this.beanId = beanId;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setDefaultPrice(Float defaultPrice) {
+    public void setDefaultPrice(BigDecimal defaultPrice) {
         this.defaultPrice = defaultPrice;
     }
 
     @Override
     public String toString() {
         return "Bean{" +
-                "id=" + id +
+                "id=" + beanId +
                 ", name='" + name + '\'' +
                 ", defaultPrice=" + defaultPrice +
                 '}';
--- server/src/main/java/com/jbe/entity/Inventory.java
+++ server/src/main/java/com/jbe/entity/Inventory.java
@@ -1,4 +1,62 @@
 package com.jbe.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Inventory {
+
+    @Id
+    @GeneratedValue
+    private int inventoryId;
+    private int investorId;
+    private int beanId;
+    private Long amount;
+
+    public Inventory(int investorId, int beanId, Long amount){
+        this.investorId = investorId;
+        this.beanId = beanId;
+        this.amount = amount;
+    }
+
+    public Inventory(int inventoryId, int investorId, int beanId, Long amount){
+        this.inventoryId = inventoryId;
+        this.investorId = investorId;
+        this.beanId = beanId;
+        this.amount = amount;
+    }
+
+    public int getInventoryId() {
+        return inventoryId;
+    }
+
+    public void setInventoryId(int inventoryId) {
+        this.inventoryId = inventoryId;
+    }
+
+    public int getInvestorId() {
+        return investorId;
+    }
+
+    public void setInvestorId(int investorId) {
+        this.investorId = investorId;
+    }
+
+    public int getBeanId() {
+        return beanId;
+    }
+
+    public void setBeanId(int beanId) {
+        this.beanId = beanId;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
 }
