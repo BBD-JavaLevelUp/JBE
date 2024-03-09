cd82c94527c49a5ce9c1a41a70fe8bd7a665b11d
Thashil Naidoo <Thashil.Naidoo@bbd.co.za>
2024-03-09 19:47:58 +0200

    Fixed server not building. Changed javax to jakarta in Entities. Changed Long to Integer in Repositories

--- server/src/main/java/com/jbe/server/repository/InvestorRepository.java
+++ server/src/main/java/com/jbe/server/repository/InvestorRepository.java
@@ -2,7 +2,9 @@ package com.jbe.server.repository;
 
 import com.jbe.server.entity.Investor;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface InvestorRepository extends CrudRepository<Investor, Long> {
+@Repository
+public interface InvestorRepository extends CrudRepository<Investor, Integer> {
 
 }
--- server/src/main/java/com/jbe/server/repository/TransactionRepository.java
+++ server/src/main/java/com/jbe/server/repository/TransactionRepository.java
@@ -2,7 +2,9 @@ package com.jbe.server.repository;
 
 import com.jbe.server.entity.Transaction;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface TransactionRepository extends CrudRepository<Transaction, Long> {
+@Repository
+public interface TransactionRepository extends CrudRepository<Transaction, Integer> {
 
 }
