5894d58654e0e5305c76c9c3bce9f917ca491cea
tebogo-bbd <159124930+tebogo-bbd@users.noreply.github.com>
2024-03-10 11:11:49 +0200

    remove unused arrayList dependency

--- client/src/main/java/com/jbe/client/MenuOptions/CreateSellOrder.java
+++ client/src/main/java/com/jbe/client/MenuOptions/CreateSellOrder.java
@@ -1,7 +1,6 @@
 package com.jbe.client.MenuOptions;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
-import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
