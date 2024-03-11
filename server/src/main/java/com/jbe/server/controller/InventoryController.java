50bf20894bdfa88571bc71826fea143eca814903
bbdnet2814 <Abdul.Osman@bbd.co.za>
2024-03-11 15:54:30 +0200

    Added inventory mappings

--- server/src/main/java/com/jbe/server/controller/InventoryController.java
+++ server/src/main/java/com/jbe/server/controller/InventoryController.java
@@ -1,9 +1,15 @@
 package com.jbe.server.controller;
 
+import com.jbe.server.entity.Inventory;
+import com.jbe.server.entity.SellOrder;
 import com.jbe.server.service.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -15,8 +21,14 @@ public class InventoryController {
         this.inventoryService = inventoryService;
     }
 
-//    @GetMapping
-//    public Optional<Inventory> getInventoryForOneUser(int userId) {
-//        return inventoryService.getInventoryForOneUser(userId);
-//    }
+    @GetMapping
+    public List<Inventory> Inventories()
+    {
+        return inventoryService.getAllInventories();
+    }
+
+    @GetMapping("/{id}")
+    public List<Inventory> inventoriesForId (@PathVariable int id){
+        return inventoryService.getInventoryForUser(id);
+    }
 }
--- server/src/main/java/com/jbe/server/service/InventoryService.java
+++ server/src/main/java/com/jbe/server/service/InventoryService.java
@@ -1,16 +1,24 @@
 package com.jbe.server.service;
 
+import com.jbe.server.entity.Inventory;
+import com.jbe.server.repository.InventoryRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class InventoryService {
-//    private final InventoryRepository inventoryRepository;
-//
-//    public InventoryService(InventoryRepository inventoryRepository) {
-//        this.inventoryRepository = inventoryRepository;
-//    }
-//
-//    public Optional<Inventory> getInventoryForOneUser(int userId) {
-//        return inventoryRepository.findById(userId);
-//    }
+    private final InventoryRepository inventoryRepository;
+
+    public InventoryService(InventoryRepository inventoryRepository) {
+        this.inventoryRepository = inventoryRepository;
+    }
+
+    public List<Inventory> getAllInventories(){
+        return inventoryRepository.findAll();
+    }
+
+    public List<Inventory> getInventoryForUser(int id) {
+        return inventoryRepository.findByInvestorId(id);
+    }
 }
