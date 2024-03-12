89b659a3fc9f35554788f220647666bd15fd236c
Thashil Naidoo <Thashil.Naidoo@bbd.co.za>
2024-03-12 16:10:55 +0200

    Added Flyway integration

---
+++ server/src/main/resources/db/migration/V1.1__add_data.sql
@@ -0,0 +1,36 @@
+/*Insert data*/
+INSERT INTO bean (bean_id, name, default_price)
+VALUES
+	(1, 'Butter', 10.50),
+	(2, 'Kidney', 8.75),
+	(3, 'Sugar', 12.00);
+
+INSERT INTO investor (investor_id, name, sa_id, email)
+VALUES
+  (1, 'John Doe', '9834567890123', 'john@yahoo.com'),
+  (2, 'Jane Smith', '9934567690123', 'jane@hotmail.com'),
+  (3, 'Michael Johnson', '0034567490123', 'michael@gmail.com');
+
+INSERT INTO buy_order (buy_order_id, investor_id, bean_id, buying_price, available_amount, total_amount, buy_order_date, is_active)
+VALUES
+  (1, 1, 1, 10.75, 100, 1000, '2024-03-08 10:00:00', true),
+  (2, 2, 2, 8.00, 150, 1500, '2024-03-08 11:00:00', true),
+  (3, 3, 3, 11.50, 200, 2000, '2024-03-08 12:00:00', true);
+
+INSERT INTO sell_order (sell_order_id, investor_id, bean_id, selling_price, available_amount, total_amount, sell_order_date, is_active)
+VALUES
+  (1, 1, 1, 12.00, 75, 750, '2024-03-08 10:30:00', true),
+  (2, 2, 2, 9.50, 100, 1000, '2024-03-08 11:30:00', true),
+  (3, 3, 3, 13.00, 125, 1250, '2024-03-08 12:30:00', true);
+
+INSERT INTO transaction (transaction_id, buy_order_id, sell_order_id, transaction_date, amount)
+VALUES
+  (1, 1, 1, '2024-03-08 10:45:00', 75),
+  (2, 2, 2, '2024-03-08 11:45:00', 100),
+  (3, 3, 3, '2024-03-08 12:45:00', 125);
+
+INSERT INTO inventory (inventory_id, investor_id, bean_id, amount)
+VALUES
+  (1, 1, 1, 500),
+  (2, 2, 2, 750),
+  (3, 3, 3, 1000);
