f43489e9dd4729914c39f93bb4246f48fc07d8cc
bbdnet2814 <Abdul.Osman@bbd.co.za>
2024-03-14 15:33:32 +0200

    Added sell order check

--- server/src/main/java/com/jbe/server/controller/SellOrderController.java
+++ server/src/main/java/com/jbe/server/controller/SellOrderController.java
@@ -150,9 +150,14 @@ public class SellOrderController {
 
     @PostMapping
     public int saveSellOrder(@RequestBody SellOrder sellOrder){
-        sellOrderService.saveOrUpdate(sellOrder);
-        matchSellOrder(sellOrder);
-        return sellOrder.getSellOrderId();
+        Inventory inventory = inventoryService.getInventoryForUserByBean(sellOrder.getInvestorId(), sellOrder.getBeanId());
+        if (sellOrder.getTotalAmount()>inventory.getAmount()) {
+            sellOrderService.saveOrUpdate(sellOrder);
+            matchSellOrder(sellOrder);
+            return sellOrder.getSellOrderId();
+        } else {
+            return 0;
+        }
     }
 
     @PutMapping
