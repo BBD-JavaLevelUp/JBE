028502645cd5d90bba6d9607baac07e014cd1cda
yashilBBD <yashil.luckan@bbd.co.za>
2024-03-14 13:34:41 +0200

    Tests

--- server/src/test/java/com/jbe/server/Controller/BeanControllerTest.java
+++ server/src/test/java/com/jbe/server/Controller/BeanControllerTest.java
@@ -2,15 +2,12 @@ package com.jbe.server.Controller;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import static org.hamcrest.Matchers.hasSize;
-import org.springframework.http.MediaType;
 import static org.hamcrest.Matchers.hasSize;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,15 +20,16 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jbe.server.controller.BeanController;
 import com.jbe.server.entity.Bean;
+import com.jbe.server.repository.BeanRepository;
 import com.jbe.server.service.BeanService;
 import com.jbe.server.service.SellOrderService;
+
+import jakarta.transaction.Transactional;
 
 @WebMvcTest(BeanController.class)
 public class BeanControllerTest
@@ -45,14 +43,16 @@ public class BeanControllerTest
   private BeanService beanService;
 
   @MockBean
+  private BeanRepository beanRepo;
+
+  @MockBean
   private SellOrderService sellOrderService;
 
   @Test
-void testGetAllBeans() throws Exception 
+  void testGetAllBeans() throws Exception 
   {
-    List<Bean> beans = List.of(new Bean(20, "TestBean1", BigDecimal.valueOf(100.00)), new Bean(21, "TestBean2", BigDecimal.valueOf(150.00))); // Create some mock beans
+    List<Bean> beans = List.of(new Bean("TestBean1", BigDecimal.valueOf(100.00)), new Bean("TestBean2", BigDecimal.valueOf(150.00))); // Create some mock beans
     when(beanService.getAllBeans()).thenReturn(beans);
-    // Add logic to mock sellOrderService as needed
 
     mockMvc.perform(get("/api/beans"))
            .andExpect(status().isOk())
@@ -62,31 +62,31 @@ void testGetAllBeans() throws Exception
   }
 
 
+  // @Test
+  // public void testGetBeans() throws Exception
+  // {
+  //   Bean bean = new Bean("TestBean22", BigDecimal.valueOf(100.00));
+
+  //   //Mockito.when(beanService.getBeanByName("TestBean")).thenReturn(bean);
+  //   beanRepo.findByName("TestBean22");
+  //   beanRepo.save(bean);
+  //   mockMvc.perform(get(requestURI))
+  //     .andExpect(content().contentType("application/json"))
+  //     .andExpect(status().isOk())
+  //     .andExpect(jsonPath("$.name", is("TestBean")))
+  //     .andDo(print());
+  // }
+
   @Test
-  public void testGetBeans() throws Exception
-  {
-    Bean bean = new Bean(20, "TestBean", BigDecimal.valueOf(100.00));
-
-    Mockito.when(beanService.getBeanById(20)).thenReturn(bean);
-
-    mockMvc.perform(get(requestURI))
-      .andExpect(content().contentType("application/json"))
-      .andExpect(status().isOk())
-      .andExpect(jsonPath("$.name", is("TestBean")))
-      .andDo(print());
-  }
-
-  @Test
-void testSaveBean() throws Exception {
-    Bean bean = new Bean(23, "TestBean3", BigDecimal.valueOf(180.00));
+  void testSaveBean() throws Exception {
+      Bean bean = new Bean("TestBean5", BigDecimal.valueOf(180.00));
     
-
-    mockMvc.perform(post("/api/beans")
+      mockMvc.perform(post("/api/beans")
             .contentType(MediaType.APPLICATION_JSON)
             .content(new ObjectMapper().writeValueAsString(bean)))
-           .andExpect(status().isOk())
-           .andExpect(content().string(String.valueOf(bean.getBeanId())));
+            .andExpect(status().isOk())
+            .andExpect(content().string(String.valueOf(bean.getBeanId())));
 
-    verify(beanService).saveOrUpdate(any(Bean.class));
-}
+      verify(beanService).saveOrUpdate(any(Bean.class));
+  }
 }
